603d980d3f35489e1ea019465f65c7cacdabd0be
Réchèr R. Réchèr <recher_burp@yahoo.fr>
2016-11-07 14:50:33 +0100

    [game-atomix-py] Script python de conversion levels kp-atomic vers PuzzleSalad. Début.

---
+++ games/atomix/kpjson_to_puzzle.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+"""
+Conversion d'un fichier JSON contenant la description de niveaux "kp-atomic" vers un fichier PuzzleSalad/PuzzleScript.
+"""
+
+# TODO : faire le code.
+
+def main():
+	print("hellow")
+
+if __name__ == '__main__':
+	main()
+
