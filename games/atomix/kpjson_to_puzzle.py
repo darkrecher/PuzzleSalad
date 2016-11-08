220dc397540e8a9e9c5c1122c5eb00e9e6f7243a
Réchèr R. Réchèr <recher_burp@yahoo.fr>
2016-11-08 12:35:32 +0100

    [game-atomix-py] Lecture du fichier json contenant les levels.

--- games/atomix/kpjson_to_puzzle.py
+++ games/atomix/kpjson_to_puzzle.py
@@ -6,8 +6,23 @@ Conversion d'un fichier JSON contenant la description de niveaux "kp-atomic" ver
 
 # TODO : faire le code.
 
+import json
+
+FILEPATH_KPATOMIC_JSON = "draknek_levels_json.js"
+
+
+def read_json_file(filepath_json):
+	with open(filepath_json, 'r', encoding='utf-8') as file_json:
+		str_json = file_json.read()
+	data_json = json.loads(str_json)
+	return data_json
+
+
 def main():
 	print("hellow")
+	kplevels_json = read_json_file(FILEPATH_KPATOMIC_JSON)
+	print(kplevels_json["levels"][1])
+
 
 if __name__ == '__main__':
 	main()
