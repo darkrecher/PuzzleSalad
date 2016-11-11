5ee5b227b282123f381cd79076fb9a1b22b5c510
Réchèr <recher_burp@yahoo.fr>
2016-11-11 15:48:38 +0100

    [game-atomix-py] Classe CharMatrix. Codage et tests des fonctions de base.

--- games/atomix/char_matrix.py
+++ games/atomix/char_matrix.py
@@ -7,9 +7,17 @@ Gestion d'un tableau de caractères en deux dimensions, un peu comme si c'était
 # TODO : fonctions pour gérer des rectangles de strings comme des matrix :
 #  - copie d'un rectangle de string dans un autre rectangle, à une coordonnée indiquée. On blitte pas les espaces.
 #  - crop d'un rectangle de string (générique : x, y, w, h).
-#  - récupération d'un char.
-#  - récupération de tous les chars autour d'un char.
+#  - recherche de tous les éléments d'un char spécifique.
 
+def _get_coords_around(coord):
+	yield (coord[0]-1, coord[1]-1)
+	yield (coord[0],   coord[1]-1)
+	yield (coord[0]+1, coord[1]-1)
+	yield (coord[0]-1, coord[1])
+	yield (coord[0]+1, coord[1])
+	yield (coord[0]-1, coord[1]+1)
+	yield (coord[0],   coord[1]+1)
+	yield (coord[0]+1, coord[1]+1)
 
 class CharMatrix():
 
@@ -32,10 +40,8 @@ class CharMatrix():
 
 	def dimensions(self):
 		"""
-		:rtype:
-			Un tuple de deux entiers
-		:return:
-			La largeur et la hauteur de la char_matrix.
+		:return: La largeur et la hauteur de la char_matrix.
+		:rtype: Un tuple de deux entiers
 		"""
 		# On part du principe que la char_matrix est correcte. Donc on peut ne consulter que la width de la première ligne.
 		# Si on n'est pas sûr que ce soit correcte, le code extérieur n'a qu'à exécuter verify_matrix.
@@ -43,9 +49,84 @@ class CharMatrix():
 			return (0, 0)
 		return (len(self.char_matrix[0]), len(self.char_matrix))
 
+	def in_dimensions(self, coord, raise_exception=False):
+		"""
+		Vérifie que les coordonnées spécifiées sont bien dans les dimensions de la matrice.
+		:param coord: coordonnée (x, y) à vérifier.
+		:param raise_exception: indique le comportement si les coordonnées spécifiées sont en dehors des dimensions de la matrice.
+		:type coord: tuple de deux entiers.
+		:type raise_exception: boolean
+		:return: True si c'est dans les dimensions, False si hors dimensions et que raise_exception=False.
+		:rtype: boolean.
+		"""
+		# FUTURE : tiens, ce serait classe un décorateur qui catche tout et qui renvoie None quand ça merde.
+		dim = self.dimensions()
+		if not(0 <= coord[0] < dim[0] and 0 <= coord[1] < dim[1]):
+			if raise_exception:
+				raise Exception(
+					"".join((
+						"Les coordonnées ",
+						str(coord),
+						" sont en dehors de la CharMatrix ",
+						str(dim),
+						"."
+					))
+				)
+			else:
+				return False
+		return True
+
+	def get_char(self, coord, raise_exception=False):
+		"""
+		Renvoie le caractère aux coordonnées spécifiées. Contrôle les coordonnées par rapport au dimensions de la matrice.
+		:param coord: coordonnée (x, y) du caractère qu'on veut récupérer dans la matrice.
+		:param raise_exception: indique le comportement si les coordonnées spécifiées sont en dehors des dimensions de la matrice.
+		:type coord: tuple de deux entiers.
+		:type raise_exception: boolean.
+		:return: le caractère demandé, ou None si hors dimensions et que raise_exception=False.
+		:rtype: une string de un seul caractère, ou None.
+		"""
+		if self.in_dimensions(coord, raise_exception):
+			# Ça va systématiquement raiser une exception si la ligne est trop courte.
+			# Ce genre de cas ne peut arriver que si la char_matrix n'est pas rectangulaire.
+			# C'est un cas anormal, qui mérite une exception.
+			# (Pour s'en prévenir, il suffit de préalablement appeler verify_matrix).
+			x, y = coord
+			return(self.char_matrix[y][x])
+		return None
+
+	def get_coords_around(self, coord):
+		"""
+		Itére sur les coordonnées autour des coordonnées spécifiées (diagonales comprises).
+		Tient compte des dimensions de la matrice.
+		:param coord: coordonnée (x, y) de la position dont on veut récupérer les coordonnées alentours.
+		:type coord: tuple de deux entiers.
+		:return: une itération sur des coordonnées
+		:rtype: tuples de deux entiers, les uns après les autres.
+		"""
+		for coord_around in _get_coords_around(coord):
+			if self.in_dimensions(coord_around):
+				yield coord_around
+
+	def get_chars_around(self, coord):
+		"""
+		Itére sur les coordonnées autour des coordonnées spécifiées (diagonales comprises) et renvoie les char correspondants.
+		Tient compte des dimensions de la matrice.
+		:param coord: coordonnée (x, y) de la position dont on veut récupérer les chars alentours.
+		:type coord: tuple de deux entiers.
+		:return: une itération sur des string de un seul caractère
+		:rtype: strings de un seul caractère, les unes après les autres.
+		"""
+		for coord_around in _get_coords_around(coord):
+			char = self.get_char(coord_around)
+			if char is not None:
+				yield char
+
 	def replace(self, before, after):
 		self.char_matrix = [
 			line.replace(before, after)
 			for line in self.char_matrix
 		]
+
+
 
--- games/atomix/test_char_matrix.py
+++ games/atomix/test_char_matrix.py
@@ -16,6 +16,52 @@ def test_dimensions():
 	cm.verify_matrix()
 	assert cm.dimensions() == (7, 3)
 
+def test_in_dimensions():
+	cm = CharMatrix( ["0123456", "abcdefg", "-------"])
+	assert cm.in_dimensions((0, 0))
+	assert cm.in_dimensions((2, 1))
+	assert cm.in_dimensions((6, 0))
+	assert cm.in_dimensions((0, 2))
+	assert not(cm.in_dimensions((-1, 0)))
+	assert not(cm.in_dimensions((0, -1)))
+	assert not(cm.in_dimensions((7, 0)))
+	assert not(cm.in_dimensions((0, 3)))
+
+def test_get_char():
+	cm = CharMatrix( ["0123456", "abcdefg", "-------"])
+	assert cm.get_char((0, 0)) == "0"
+	assert cm.get_char((2, 1)) == "c"
+	assert cm.get_char((6, 0)) == "6"
+	assert cm.get_char((0, 2)) == "-"
+	assert cm.get_char((-1, 0)) is None
+	assert cm.get_char((0, -1)) is None
+	assert cm.get_char((7, 0)) is None
+	assert cm.get_char((0, 3)) is None
+
+def test_get_coords_around():
+	cm = CharMatrix( ["0123456", "abcdefg", "-------", "*******"])
+	assert tuple(cm.get_coords_around((2, 2))) == (
+		(1, 1), (2, 1), (3, 1),
+		(1, 2), (3, 2),
+		(1, 3), (2, 3), (3, 3),
+	)
+	assert tuple(cm.get_coords_around((0, 0))) == (
+		(1, 0),
+		(0, 1), (1, 1),
+	)
+	assert tuple(cm.get_coords_around((6, 3))) == (
+		(5, 2), (6, 2),
+		(5, 3),
+	)
+	assert tuple(cm.get_coords_around((10, 10))) == ()
+
+def test_get_chars_around():
+	cm = CharMatrix( ["0123456", "abcdefg", "tuvwxyz", "/*-+,.!"])
+	assert "".join(cm.get_chars_around((2, 2))) == "bcduw*-+"
+	assert "".join(cm.get_chars_around((0, 0))) == "1ab"
+	assert "".join(cm.get_chars_around((6, 3))) == "yz."
+	assert "".join(cm.get_chars_around((10, 10))) == ""
+
 def test_replace():
 	cm = CharMatrix( ["001", "210"])
 	cm.replace('1', 'z')
@@ -24,5 +70,3 @@ def test_replace():
 		"2z0",
 	))
 	assert str(cm) == str_cm_final
-
-
